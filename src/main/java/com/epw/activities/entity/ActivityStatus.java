cd96ee4257c28540e4628dd3d97f9d04d5f302d7
Julian <florezjulian74@udenar.edu.co>
2026-03-25 07:52:32 -0500

    chore: crear rama solo con archivos de conexion BD

--- src/main/java/com/epw/activities/entity/ActivityStatus.java
+++
@@ -1,8 +0,0 @@
-package com.epw.activities.entity;
-
-public enum ActivityStatus {
-    BACKLOG,
-    IN_PROGRESS,
-    DONE,
-    ARCHIVED
-}
--- src/main/java/com/epw/activities/repository/ActivityDetailRepository.java
+++
@@ -1,8 +0,0 @@
-package com.epw.activities.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.epw.activities.entity.ActivityDetail;
-
-public interface ActivityDetailRepository extends JpaRepository<ActivityDetail, Long> {
-}
